63633c8838347740df075a31951033799e4df96c
Anurag_M <anurag9996211@gmail.com>
2024-09-17 11:34:08 +0530

    setup.py and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,32 @@
+'''
+helps in creating ML application as a package, 
+helps in deployment,  
+which can be installed by others or the user itself 
+'''
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+def get_requirements(path:str)->List[str]:
+    '''
+    returns a list of requirements
+    '''
+    requirements=[]
+    with open(path) as f:
+        requirements=f.readlines()
+        requirements=(req.replace("\n", "")
+                      for req in requirements)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+# project metadata
+setup(
+    name='Project_One',
+    version='0.0.1',
+    author='Anurag',
+    author_email='anurag9996211@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
