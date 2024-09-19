9d8e90bbc3f0b276176730ca27db6583e65bdc60
Anurag_M <anurag9996211@gmail.com>
2024-09-19 13:56:46 +0530

    Added data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,8 @@
+# DI pipeline to read Dset, split in train-test and save these along with raw data
+# .. while logging each step of the process
 import os 
 import sys
+from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -8,41 +11,45 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
-class DataIngestionConfig:
+class DataIngestionConfig: # dataclass that defines file paths for storing ingested data, stores in the 'artifacts' folder
     train_data_path: str = os.path.join("artifacts", "train.csv")
     test_data_path: str = os.path.join("artifacts", "test.csv")
     raw_data_path: str = os.path.join("artifacts", "data.csv")
 
-class DataIngestion:
-    def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
+class DataIngestion: # DI class handles DI process, constructor initializes of DIConfig 
+    def __init__(self): 
+        self.ingestion_config = DataIngestionConfig() 
 
-    def initiate_data_ingestion(self):
+    def initiate_data_ingestion(self): # method performs the actual DI process
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\StudentsPerformance.csv')
+            df = pd.read_csv(r'notebook\data\StudentsPerformance.csv') # read .csv to dataframe 
+            logging.info("Read the datset as Dataframe") 
             
-            logging.info("Read the datset as Dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True) # create dirs for storing data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True) # raw data saved as CSV in the path specified
             
             logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True) # saving train data as csv in paths specfied
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True) # saving test data..
 
             logging.info("Ingestion of the data has been completed")
 
             return(
-                self.ingestion_config.train_data_path,
+                self.ingestion_config.train_data_path, # return train & test file paths
                 self.ingestion_config.test_data_path,
             )
-        except Exception as e:
-            raise CustomException(e, sys)
+        except Exception as e: 
+            raise CustomException(e, sys) # if exception occurs, it's caught and raised as CE, includes addn'l errorhandling and logging
         
 if __name__ == "__main__":
     obj = DataIngestion()
     obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
 
 
---
+++ src/components/data_transformation.py
@@ -0,0 +1,116 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+# @dataclass decorator automatically generates several special methods for the class, 
+# ...such as __init__(), __repr__(), and __eq__(). This saves you from writing boilerplate code (repetitive code)
+# Additionally, if you needed to add more configuration parameters in the future, 
+# ...using @dataclass would make it very easy to do so without having to modify an __init__ method.
+@dataclass 
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts", "proprocessor.pkl") # define path for saving preprocessor object, Without @dataclass, you would need to write an __init__ method explicitly:
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for creating .pkl files 
+        for data transformation
+        '''
+        try:
+            numerical_columns = ["writing score", "reading score"] # define num & cat columns
+            categorical_columns = [
+                "gender",
+                "race/ethnicity",
+                "parental level of education",
+                "lunch",
+                "test preparation course",
+            ]
+            # create pipeline for num features
+            num_pipeline = Pipeline(
+                steps=[
+                ("imputer", SimpleImputer(strategy="median")), # handle missing values by imputing with median, coz: outliers
+                ("scaler", StandardScaler()) # standardize features by subtra mean & div by variance
+                ]
+            )
+            # create pipeline for cat features
+            cat_pipeline = Pipeline(
+
+                steps=[
+                ("imputer", SimpleImputer(strategy="most_frequent")), # impute with most freq value
+                ("one_hot_encoder", OneHotEncoder()), # encode as one-hot numeric array
+                ("scaler", StandardScaler(with_mean=False)) # False: coz data is sparse after one-hot coding
+                ]
+            )
+            logging.info(f"Categorical columns: {categorical_columns}") # Log column information
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            # Combine num & cat pipelines
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipeline", num_pipeline, numerical_columns),
+                ("cat_pipelines", cat_pipeline, categorical_columns)
+                ]
+            )
+            return preprocessor        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self, train_path, test_path): # Inputs from data_ingestion.py
+        try:
+            train_df = pd.read_csv(train_path) # Read train & test data
+            test_df = pd.read_csv(test_path)
+            logging.info("Reading of train & test data has been completed")
+
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "math score"
+            numerical_columns = ["writing_score", "reading_score"]
+
+            # separate features and targets
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            # Combine preprocessed features with target variable
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+            # Saving the preprocessing object .pkl  
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+            )
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
